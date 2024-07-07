37bfd02fb8af3cae802cc9b433f51abfff1ef301
Aniruddha-L <aniruddhanarasimman@gmail.com>
2024-07-07 14:37:04 +0530

    done jumbo menu

--- frontend/src/Components/Item.jsx
+++ frontend/src/Components/Item.jsx
@@ -9,6 +9,7 @@ import kpr from '../Static/Asstes/100 ml/Kesar Pista Royale.png'
 import raj from '../Static/Asstes/100 ml/Rajbhoj.png'
 import vr from '../Static/Asstes/100 ml/Vanilla Royale.png'
 import cup_straw from '../Static/Asstes/65 ml/Strawberry.png'
+
 import combo_adf from '../Static/Asstes/Combo/Afghan dry fruit.png'
 import combo_mango from '../Static/Asstes/Combo/Alphonso mango.png'
 import combo_bc from '../Static/Asstes/Combo/Black current.png'
@@ -21,6 +22,16 @@ import combo_sscb from  '../Static/Asstes/Combo/sscb.png'
 import combo_straw from "../Static/Asstes/Combo/Strawberry.png"
 import combo_tf from  "../Static/Asstes/Combo/Tutti Frutti.png"
 import combo_van from  "../Static/Asstes/Combo/Vanilla.png"
+
+import jumbo_bb from '../Static/Asstes/Jumbo Cup/Butterscotch Bliss.png'
+import jumbo_cb from '../Static/Asstes/Jumbo Cup/Chocolate Brownie.png'
+import jumbo_fnnf from '../Static/Asstes/Jumbo Cup/Fruit N Nut Fantasy.png'
+import jumbo_gp from '../Static/Asstes/Jumbo Cup/Golden Pearl.png'
+import jumbo_h from '../Static/Asstes/Jumbo Cup/Haldi.png'
+import jumbo_raj from '../Static/Asstes/Jumbo Cup/Rajbhoj.png'
+import jumbo_ka from '../Static/Asstes/Jumbo Cup/King Alphonso.png'
+import jumbo_pn from '../Static/Asstes/Jumbo Cup/Pan-Nawabi.png'
+import jumbo_sh from '../Static/Asstes/Jumbo Cup/Shalimar.png'
 
 function Item({name,size,  price, image}) {
   var source;
@@ -90,6 +101,33 @@ function Item({name,size,  price, image}) {
       break
     case 22:
       source = combo_van
+      break
+    case 23:
+      source = jumbo_bb
+      break
+    case 24:
+      source = jumbo_cb
+      break
+    case 25:
+      source = jumbo_fnnf
+      break
+    case 26:
+      source = jumbo_gp
+      break
+    case 27:
+      source = jumbo_h
+      break
+    case 28:
+      source = jumbo_ka
+      break
+    case 29:
+      source = jumbo_pn
+      break
+    case 30:
+      source = jumbo_raj
+      break
+    case 31:
+      source = jumbo_sh
       break
   }
   
--- frontend/src/Pages/Product/Jumbo.jsx
+++ frontend/src/Pages/Product/Jumbo.jsx
@@ -1,12 +1,41 @@
 import React from 'react'
 import Menu from '../../Components/Menu'
+import Item from '../../Components/Item'
+import '../../Static/Style/Jumbo.css'
 
 function Jumbo(){
+  document.title='Order- Jumbo Cup'
   return (
-    <div>
+    <div className='Jumbo'>
       <Menu></Menu>
-      <ul>
-        <li></li>
+      <ul className='jumbo_display'>
+        <li>
+          <Item name={'Butterscotch Bliss'} size={'125ml'} price={'100'} image={23} ></Item>
+        </li>
+        <li>
+          <Item name={'Chocolate Brownie'} size={'125ml'} price={'100'} image={24} ></Item>
+        </li>
+        <li>
+          <Item name={'Fruit N Nut Fantasy'} size={'125ml'} price={'100'} image={25} ></Item>
+        </li>
+        <li>
+          <Item name={'Golden Pearl'} size={'125ml'} price={'100'} image={26} ></Item>
+        </li>
+        <li>
+          <Item name={'Haldi'} size={'125ml'} price={'100'} image={27} ></Item>
+        </li>
+        <li>
+          <Item name={'King Alphonso'} size={'125ml'} price={'100'} image={28} ></Item>
+        </li>
+        <li>
+          <Item name={'Pan-Nawabi'} size={'125ml'} price={'100'} image={29} ></Item>
+        </li>
+        <li>
+          <Item name={'Rajbhoj'} size={'125ml'} price={'100'} image={30} ></Item>
+        </li>
+        <li>
+          <Item name={'Shalimar'} size={'125ml'} price={'100'} image={31} ></Item>
+        </li>
       </ul>
     </div>
   )
